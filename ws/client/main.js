ca37fc9b4f007c63682d4b93321be262814ca34b
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-10-11 00:37:44 +0300

    add simple example with chat

--- ws/client/main.js
+++ ws/client/main.js
@@ -1,15 +1,38 @@
-'use strict';
+'usen strict';
+
+const CHAR_RETURN = 13;
 
 const main = () => {
-  const ws = new WebSocket('ws://127.0.0.1:8000');
-  ws.onopen = () => {
-    const timer = setInterval(() => {
-      const data = { date: Date.now() };
-      ws.send(JSON.stringify(data));
-    }, 1000);
-    ws.onmessage = ({ data }) => void console.log({ data });
-    ws.onclose = () => void clearInterval(timer);
+  const socket = new WebSocket('ws://127.0.0.1:8000/chat');
+  const chat = document.getElementById('chat');
+  const msg = document.getElementById('msg');
+  msg.focus();
+
+  const writeLine = (text) => {
+    const line = document.createElement('div');
+    line.innerHTML = `<p>${text}</p>`;
+    chat.appendChild(line);
   };
+
+  socket.addEventListener('open', () => {
+    writeLine('connected');
+  });
+
+  socket.addEventListener('close', () => {
+    writeLine('closed');
+  });
+
+  socket.addEventListener('message', ({ data }) => {
+    writeLine(data);
+  });
+
+  msg.addEventListener('keydown', (event) => {
+    if (event.key !== 'Enter') return;
+    const { value: string } = msg;
+    msg.value = '';
+    writeLine(string);
+    socket.send(string);
+  });
 };
 
 main();
